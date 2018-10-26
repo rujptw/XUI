33aa8679b8cdeda7f20c84657c2f1f5ad7dc982a
rujptw <2609274343@qq.com>
2018-10-26 18:07:27 +0800

    配置基础css

--- src/source/components/click-slider/click-slider.vue
+++ src/source/components/click-slider/click-slider.vue
@@ -1,26 +1,24 @@
 <template>
- <div class="">
-
+ <div class="xui-click_slider">
+     <span class="xui__span--normal">近20日</span>
+     <span class="xui__span--normal">近十二月</span>
+     <span class="xui__span--normal">年</span>
  </div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
- name:"XClickSlider",
- data() {
- return {
-
- }
- },
- props: {
-
- },
- methods:{
-     getFunc(){
-         console.log("you are my Coke")
-     }
- }
-}
+  name: "XClickSlider",
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {
+    getFunc() {
+      console.log("you are my Coke");
+    }
+  }
+};
 </script>
 
 <style lang="stylus">

139fbdad7effd55f410265ef530edb54a3afbf5c
rujptw <2609274343@qq.com>
2018-12-28 14:09:05 +0800

    增加router-config注释

--- examples/router/router-config.js
+++ examples/router/router-config.js
@@ -1,5 +1,4 @@
-/**
-利用webpack的require.context生成路由数组
+/**利用webpack.require.context生成路由数组
 */
 let context = require.context("@/docs/", true, /\.md$/);
 let files = context.keys();
@@ -12,6 +11,7 @@ export const componentRouters = files.map(file => {
   return {
     name,
     path,
+    //Babel@6 导出的是一个{default:componentName},所以要加default处理
     component: require(`@/docs/${component}`).default
   };
 });
